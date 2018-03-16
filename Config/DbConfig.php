162d860b2999af7c9e1a4369c2c7a4a50b3c32f8
svetoslav.bozhinov <sbojinov@yahoo.com>
2018-03-16 14:02:10 +0200

    changes in the Config folder

--- Config/DbConfig.php
+++ Config/DbConfig.php
@@ -7,6 +7,6 @@ class DbConfig
     const DB_HOST = 'localhost';
     const DB_USER = 'root';
     const DB_PASSWORD = '';
-    const DB_NAME = 'company_v2';
+    const DB_NAME = 'q1q1eu2x_company';
 
 }
