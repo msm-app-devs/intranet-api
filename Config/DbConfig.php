08ed24b8b5d654d9486e5216a7e0267a4aaacddf
Svetoslav <sbojinov@yahoo.com>
2018-05-29 00:20:43 +0300

    New version

---
+++ Config/DbConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Employees\Config;
+
+class DbConfig
+{
+    const DB_HOST = 'localhost';
+    const DB_USER = 'q1q1eu2x';
+    const DB_PASSWORD = 'f!45EIqkBj,P';
+    const DB_NAME = 'q1q1eu2x_company';
+}
---
+++ Config/DefaultParam.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Employees\Config;
+
+class DefaultParam
+{
+    const DefaultController = "users";
+    const DefaultAction = "login";
+    const ServerRoot = "http://q1q1.eu/employees/";
+    const EmployeeContainer = "webroot/images/";
+    const NewsImageContainer = "webroot/news/";
+}
